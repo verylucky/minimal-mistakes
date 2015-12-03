c39f17f05c234001a1819de4afbbe4387daeb82f
junling <526093880@qq.com>
2015-12-03 17:14:29 +0800

    change blog

--- gitpush.sh
+++ gitpush.sh
@@ -1,7 +1,8 @@
 sed 's/127.0.0.1:4000/verylucky.github.io/g' _config.yml > tmpfile
+mv tmpfile _config.yml
 ./build.sh
 git add -A
 git commit -m "change blog"
 git push origin master
-sed 's/verylucky.github.io/127.0.0.1:4000/g' tmpfile > _config.yml
-rm tmpfile
+sed 's/verylucky.github.io/127.0.0.1:4000/g' _config.yml > tmpfile
+mv tmpfile _config.yml
