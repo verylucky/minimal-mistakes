1d9b739d8109854858e6f2415e8a1a1ee0d9af02
junling <526093880@qq.com>
2015-12-03 17:05:12 +0800

    change blog

--- gitpush.sh
+++ gitpush.sh
@@ -1,4 +1,6 @@
+sed 's/127.0.0.1/verylucky.github.io/g' _config.yml > _config.yml
 ./build.sh
 git add -A
 git commit -m "change blog"
 git push origin master
+sed 's/verylucky.github.io/127.0.0.1/g' _config.yml > _config.yml
